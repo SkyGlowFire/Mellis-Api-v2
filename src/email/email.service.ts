0a3b2d4c64e37d3726c0b9d685ce25028142b6aa
SkyGlowFire <SkyGlowFire@gmail.com>
2021-12-20 17:20:52 +0700

    changed whitelist

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -21,7 +21,6 @@ export class EmailService {
                 auth: {
                     api_key: this.configService.get('MAILGUN_API_KEY'),
                     domain: this.configService.get('MAILGUN_DOMAIN'),
-                    host: "api.eu.mailgun.net"
                 }
             }
         ));
